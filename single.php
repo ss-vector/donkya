7d36996ce894d015b14f8d0ef378f687d090f705
ss-vector <omar_mejia@hotmail.com>
2022-05-15 21:41:01 -0500

    random update

--- single.php
+++ single.php
@@ -6,24 +6,27 @@
 get_header(); ?>
 <div class="single-page-wrapper">
 	<div id="primary" class="container">
-		<main id="main" class="site-main">
-			<?php 
-				if( have_posts() ):
-					while( have_posts() ): the_post(); ?>
-						<article class="single_post_layout">
-							<div class="blog-title">
-								<?php the_title( '<h2>', '</h2>'); ?>
-								<span><?php the_category( ' ' ); ?></span>
-							</div>
-							
-							<div class="post-content">
-								<?php the_content(); ?>
-							</div>
-						</article>
-					<?php
-					endwhile;
-				endif;
-			?>
+		<main id="main" class="site-main row mt-5">
+
+			<div class="content-wrapper">
+				<?php 
+					if( have_posts() ):
+						while( have_posts() ): the_post(); ?>
+							<article class="single_post_layout">
+								<div class="blog-title">
+									<?php the_title( '<h2>', '</h2>'); ?>
+									<span><?php the_category( ' ' ); ?></span>
+								</div>
+								
+								<div class="post-content">
+									<?php the_content(); ?>
+								</div>
+							</article>
+						<?php
+						endwhile;
+					endif;
+				?>
+			</div>
 		</main>
 	</div>
 </div>
