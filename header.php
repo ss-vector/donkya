a070d2316823c56844bbc459c6e745f4891a8f52
ss-vector <omar_mejia@hotmail.com>
2022-06-11 16:53:48 -0500

    random update

--- header.php
+++ header.php
@@ -50,22 +50,7 @@
 		          			'walker'          	=> New header_menu_walker(),
 		          		) );
 		          	?>
-		            <!-- ref. 
-		            <ul class="navbar-nav ml-auto">
-		              <li class="nav-item active">
-		                <a class="nav-link" href="index.html">Home</a>
-		              </li>
-		              <li class="nav-item">
-		                <a class="nav-link" href="about.html">About Us</a>
-		              </li>
-		              <li class="nav-item">
-		                <a class="nav-link" href="services.html">Our Services</a>
-		              </li>
-		              <li class="nav-item">
-		                <a class="nav-link" href="contact.html">Contact Us</a>
-		              </li>
-		            </ul>
-		        	-->
+
 		          </div>
 
 		        </div>
