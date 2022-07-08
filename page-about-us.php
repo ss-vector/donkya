c22ddc290a10d280f20a46fd82a30551c8a4fedb
ss-vector <omar_mejia@hotmail.com>
2022-07-07 21:35:10 -0500

    about-us page reworked

--- page-about-us.php
+++ page-about-us.php
@@ -1,10 +1,27 @@
-<?php get_header();
+<?php get_header(); ?>
 
-?>
+<div class="page-wrapper">
+	<div id="primary" class="container">
+		<main id="page" class="site-main row mt-5">
 
-<div class="container">
-	<h2>Donkya Plastics</h2>
-	
+			<div class="col-xl-12 content-wrapper">
+					<div class="section-wrapper">
+						<h2 class="display-4 fw-bold">Us</h2>
+						<div class="container-fluid py-5">
+							<h1 class="display-5 fw-bold">Donkya Plastics</h2>
+							<p class="col-md-8 fs-4">We work as a family providing ideas for better living that go beyond our ideologies.</p>
+						</div>
+						
+					</div>
+
+					<div class="section-wrapper">
+						<h3>Our Initiative</h3>
+						<p class="col-md-8 fs-4">We know that our story so far went from being nomads to having the power to shape our enviroment, and nowadays problem of pollution is putting us in a hurry.</p>
+						
+					</div>
+			</div>
+		</main>
+	</div>	
 </div>
 
 <?php get_footer(); ?>
