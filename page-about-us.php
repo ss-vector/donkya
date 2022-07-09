c40939d04a8996115e6eb91cf0cb539d7706e5af
ss-vector <omar_mejia@hotmail.com>
2022-07-09 17:57:49 -0500

    about us page update

--- page-about-us.php
+++ page-about-us.php
@@ -1,24 +1,27 @@
 <?php get_header(); ?>
 
 <div class="page-wrapper">
-	<div id="primary" class="container">
-		<main id="page" class="site-main row mt-5">
-
-			<div class="col-xl-12 content-wrapper">
-					<div class="section-wrapper">
-						<h2 class="display-4 fw-bold">Us</h2>
+	<div id="primary" class="site-main">
+		<main id="page" class="container">
+			<div class="section-wrapper row main">
+				<div class="col-xl-12">
+					<div class="mt-5">
+						<h2 class="display-4 fw-bold text-center">About Us</h2>
 						<div class="container-fluid py-5">
-							<h1 class="display-5 fw-bold">Donkya Plastics</h2>
-							<p class="col-md-8 fs-4">We work as a family providing ideas for better living that go beyond our ideologies.</p>
+							<img src="" height="100">
+							<h1 class="display-6 fw-bold">Donkya Plastics</h1>
+							<p class="col-md-6 fs-4">We work as a family providing ideas for better living that go beyond our ideologies.</p>
+							<p class="col-md-8 fs-4"><strong>DONKYA</strong> was born out of an interest in restoring and preserving the environment. As a company, we are dedicated to constructing products from recycled plastic that add durability and style to our designs.</p>
+							<p>With recycled material we could do lots of stuff, we need to share the value left in the trash.</p>
 						</div>
-						
 					</div>
-
-					<div class="section-wrapper">
-						<h3>Our Initiative</h3>
-						<p class="col-md-8 fs-4">We know that our story so far went from being nomads to having the power to shape our enviroment, and nowadays problem of pollution is putting us in a hurry.</p>
-						
-					</div>
+				</div>
+			</div>
+			<div class="section-wrapper">
+				<div class="container-fluid py-5">
+					<h2 class="display-6 fw-bold">Our Initiative</h2>
+					<p class="col-md-8 fs-4">We know that our story so far went from being nomads to having the power to shape our enviroment, and nowadays problem of pollution is putting us in a hurry.</p>
+				</div>
 			</div>
 		</main>
 	</div>	
