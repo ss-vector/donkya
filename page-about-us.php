c7f59053a33f57072c770312db71fbb905247f4a
ss-vector <omar_mejia@hotmail.com>
2022-07-09 19:20:09 -0500

    random fixes

--- page-about-us.php
+++ page-about-us.php
@@ -8,7 +8,6 @@
 					<div class="mt-5">
 						<h2 class="display-4 fw-bold text-center">About Us</h2>
 						<div class="container-fluid py-5">
-							<img src="" height="100">
 							<h1 class="display-6 fw-bold">Donkya Plastics</h1>
 							<p class="col-md-6 fs-4">We work as a family providing ideas for better living that go beyond our ideologies.</p>
 							<p class="col-md-8 fs-4"><strong>DONKYA</strong> was born out of an interest in restoring and preserving the environment. As a company, we are dedicated to constructing products from recycled plastic that add durability and style to our designs.</p>
@@ -21,6 +20,23 @@
 				<div class="container-fluid py-5">
 					<h2 class="display-6 fw-bold">Our Initiative</h2>
 					<p class="col-md-8 fs-4">We know that our story so far went from being nomads to having the power to shape our enviroment, and nowadays problem of pollution is putting us in a hurry.</p>
+					<p class="col-md-8 fs-4">Through our local recycling solution, we create awareness and additional economic value for local communities. Furthermore, part of the revenue is used for renaturation and reforestation.</p>
+				</div>
+			</div>
+			<div class="section-wrapper">
+				<div class="row align-items-md-strech">
+					<div class="col-md-6">
+						<div class="h-100 p-5 text-white bg-dark rounded-3">
+							<h2>Mission</h2>
+							<p>Here at DONKYA Plastic strive to protect nature in combination with a sustainable economy. By managing recycled plastics and transforming them into a handmade and innovative product. Our goals are based on solidarity and care for the planet in the face of the growing problem caused by the pollution of different types of plastic.</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="h-100 p-5 bg-light border rounded-3">
+							<h2>Vision</h2>
+							<p>Our vision is to be an environmental reference from the local to the international level, characterised by an environmental philosophy not only in the social environment but also in the promotion of a green economy.</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</main>
