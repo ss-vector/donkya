99de89b624a76611437745eae39125a9ef4b8b64
ss-vector <omar_mejia@hotmail.com>
2022-07-19 20:06:41 -0500

    about donkya plastics structure rework 1

--- page-about-us.php
+++ page-about-us.php
@@ -2,21 +2,23 @@
 
 <div class="page-wrapper">
 	<div id="primary" class="site-main">
-		<main id="page" class="container">
+		<div class="container-fluid">
 			<div class="section-wrapper row main">
-				<div class="col-xl-12">
+				<div class="col-xl-12 mt-5">
 					<div class="mt-5">
-						<h2 class="display-4 fw-bold text-center">About Us</h2>
-						<div class="container-fluid py-5">
-							<h1 class="display-6 fw-bold">Donkya Plastics</h1>
+						<h2 class="display-4 fw-bold text-center">About Donkya Plastics</h2>
+						<div class="container py-5">
+							<!--h1 class="display-6 fw-bold">Donkya Plastics</h1-->
 							<blockquote class="blockquote">We work as a family providing ideas for better living that go beyond our ideologies.</blockquote>
-							<p class="col-md-8 fs-4"><strong>DONKYA</strong> was born out of an interest in restoring and preserving our natural environments.</p>
-							<p class="col-md-8 fs-6">With recycled material we could do lots of stuff, we need to share the value left in the trash.</p>
+							<p class="col-md-8 text-dark fs-4"><strong>DONKYA</strong> was born out of an interest in restoring and preserving our natural environments.</p>
+							<p class="col-md-8 text-dark fs-4">With recycled material we could do lots of stuff, we need to share the value left in the trash.</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="section-wrapper">
+		</div>
+		<main id="page" class="container">
+			<div class="section-wrapper row">
 				<div class="container-fluid py-5">
 					<p class="col-md-8 fs-4">We know that our story so far went from being nomads to having the power to shape our enviroment, and nowadays problem of pollution is putting us in a hurry.</p>
 				</div>
@@ -24,9 +26,9 @@
 			<div class="section-wrapper">
 				<div class="row align-items-md-strech">
 					<div class="col-md-6">
-						<div class="h-100 p-5 text-white bg-dark rounded-3">
+						<div class="h-100 p-5 text-white bg-green rounded-3">
 							<h2>Mission</h2>
-							<p class="fs-6">Here at <strong>DONKYA Plastics</strong> strive to protect nature in combination with a sustainable economy. By managing recycled plastics and transforming them into a handmade and innovative product. Our goals are based on solidarity and care for the planet in the face of the growing problem caused by the pollution of different types of plastic.</p>
+							<p class="fs-6 text-white">Here at <strong>DONKYA Plastics</strong> strive to protect nature in combination with a sustainable economy. By managing recycled plastics and transforming them into a handmade and innovative product. Our goals are based on solidarity and care for the planet in the face of the growing problem caused by the pollution of different types of plastic.</p>
 						</div>
 					</div>
 					<div class="about-vision-box col-md-6">
@@ -60,6 +62,11 @@
 						<p class="fs-6">The process goes from collecting the plastic waste to cleaning, shredding and transforming it into new, stylish and more durable products made from recycled plastic. Manual and artesanal processes are used, as well as tech with the use of machines such as “PetBots”, 3D printers, shredders, plate press and injection machines.</p>
 						<p class="fs-6">Furthermore, we create more environmental awareness and knowledge around the topic of plastic and resource conservation through workshops and courses.</p>
 					</div>
+			</div>
+			<div class="p-5 text-center">
+				<button type="button" class="btn btn-success">
+					Products
+				</button>
 			</div>
 		</main>
 	</div>	
