6baf9d34f1e3bdb9f5dd591c3b7a50bf706bc2b8
ss-vector <omar_mejia@hotmail.com>
2022-08-18 12:44:01 -0500

    footer update- sass rework

--- footer.php
+++ footer.php
@@ -40,33 +40,35 @@
 		          </div>
 		          
 		          <div class="col-md-3 col-sm-6 col-xs-12">
-		            <div class="footer-item">
-		              <div class="footer-heading">
-		                <h2><?php _e( 'Contact Information', 'donkyatheme' ); ?></h2>
-		              </div>
-		              <ul class="social-icons p-3">
-		              	<li>
-		              		<a href="https://www.facebook.com/Donkya-plastic-108646218567740/" target="_blank">
-		              			<i class="fa-brands fa-facebook fa-xl"></i>
-		              		</a>
-		              	</li>
-		              	<li>
-		              		<a href="https://twitter.com/DonkyaF" target="_blank">
-		              			<i class="fa-brands fa-twitter fa-xl"></i>
-		              		</a>		              		
-		              	</li>
-		              	<li>
-		              		<a href="#" target="_blank">
-		              			<i class="fa-brands fa-youtube fa-xl"></i>
-		              		</a>		              		
-		              	</li>
-		              </ul>
+
+			    	<div class="footer-heading">
+		                	<h2><?php _e( 'Contact Information', 'donkyatheme' ); ?></h2>
+			            </div>
+		              	<div class="footer-social-icons">
+			              <ul class="social-icons p-3">
+			              	<li>
+			              		<a href="https://www.facebook.com/Donkya-plastic-108646218567740/" target="_blank">
+			              			<i class="fa-brands fa-facebook fa-xl"></i>
+			              		</a>
+			              	</li>
+			              	<li>
+			              		<a href="https://twitter.com/DonkyaF" target="_blank">
+			              			<i class="fa-brands fa-twitter fa-xl"></i>
+			              		</a>		              		
+			              	</li>
+			              	<li>
+			              		<a href="https://www.youtube.com/channel/UCwrlylJEG0t3tzJj5E-EZIw/featured" target="_blank">
+			              			<i class="fa-brands fa-youtube fa-xl"></i>
+			              		</a>		              		
+			              	</li>
+			              </ul>
+		          		</div>
 		              <ul class="footer-list">
 		                <li><?php _e( 'Phone', 'donkyatheme' ); ?>:<a href="#"> +43 677 64063544</a></li>
 		                <li><?php _e( 'Contact', 'donkyatheme' ); ?>: <a href="#">info@donkya.com</a></li>
 		              </ul>
-		            </div>
-		          </div>
+
+		          	</div>
 		          <div class="col-md-12">
 		            <div class="sub-footer">
 		              <p>Copyright &copy; 2022 Donkya Plastic
